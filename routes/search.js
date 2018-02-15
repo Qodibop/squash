e8842dbf6521828d8ab83c1827b365d29b5bfe0c
Jean-Nicolas Piketty <jnpiketty@BDLCM.local>
2018-02-15 14:39:10 +0100

    Matching with facilities

--- routes/search.js
+++ routes/search.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const Search = require("../models/search");
+const Facility = require("../models/facility");
 const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
 
 router.get("/search", ensureLoggedIn(), (req, res, next) => {
-  res.render("searches/search", { matching: [] });
+  res.render("searches/search", {
+    messages: [],
+    facilityResults: [],
+    searchResults: []
+  });
 });
 
 router.post("/search", ensureLoggedIn(), (req, res, next) => {
@@ -25,39 +30,83 @@ router.post("/search", ensureLoggedIn(), (req, res, next) => {
     time
   });
   const matchingSearch = [];
+  const matchingFacility = [];
 
   search.save(err => {
     if (err) return next(err);
   });
 
-  Search.find({}, (err, searches) => {
-    for (let i = 0; i < searches.length; i++) {
-      if (
-        zipcode === searches[i].zipcode &&
-        day === searches[i].day &&
-        time === searches[i].time
-      ) {
-        if (search.userId.toString() !== searches[i].userId.toString()) {
-          console.log(search.userId + "search");
-          console.log(searches[i].userId);
-          matchingSearch.push(searches[i]);
+  Promise.all([Search.find(), Facility.find()])
+    .then(values => {
+      let searches = values[0];
+      let facilities = values[1];
+      for (let i = 0; i < searches.length; i++) {
+        if (zipcode === searches[i].zipcode && day === searches[i].day && time === searches[i].time) {
+          if (search.userId.toString() !== searches[i].userId.toString()) {
+            console.log(search.userId + "search");
+            console.log(searches[i].userId);
+            matchingSearch.push(searches[i]);
+          }
         }
       }
-    }
-    if (matchingSearch.length === 0) {
-      console.log("No matching research");
+      for (let i = 0; i < facilities.length; i++) {
+        if (zipcode === facilities[i].zipcode) {
+          matchingFacility.push(facilities[i]);
+        }
+      }
       res.render("searches/search", {
-        matching: [
-          {
-            username: "No matching, your request has been saved!",
-            userPic: "/images/noMatches.jpg"
-          }
-        ]
+        messages: ["Sorry dude, no facility in this area!", "No matching but your request has been saved!"],
+        facilityResults: matchingFacility,
+        searchResults: matchingSearch
       });
-    } else {
-      res.render("searches/search", { matching: matchingSearch });
-    }
-  });
+    })
+    .catch(err => console.log(err));
+
+  // Search.find({}, (err, searches) => {
+  //   for (let i = 0; i < searches.length; i++) {
+  //     if (zipcode === searches[i].zipcode && day === searches[i].day && time === searches[i].time) {
+  //       if (search.userId.toString() !== searches[i].userId.toString()) {
+  //         console.log(search.userId + "search");
+  //         console.log(searches[i].userId);
+  //         matchingSearch.push(searches[i]);
+  //       }
+  //     }
+  //   }
+  //   if (matchingSearch.length === 0) {
+  //     console.log("No matching research");
+  //     res.render("searches/search", {
+  //       matching: [
+  //         {
+  //           username: "No matching, your request has been saved!",
+  //           userPic: "/images/noMatches.jpg"
+  //         }
+  //       ]
+  //     });
+  //   } else {
+  //     res.render("searches/search", { matching: matchingSearch });
+  //   }
+  // });
+
+  // Facility.find({}, (err, facilities) => {
+  //   for (let i = 0; i < facilities.length; i++) {
+  //     if (zipcode === facilities[i].zipcode) {
+  //       matchingFacility.push(facilities[i]);
+  //     }
+  //   }
+  //   if (matchingFacility.length === 0) {
+  //     console.log("No matching research");
+  //     res.render("searches/search", {
+  //       facilityResults: [
+  //         {
+  //           name: "No matching, your request has been saved!",
+  //           userPic: "/images/noMatches.jpg"
+  //         }
+  //       ]
+  //     });
+  //   } else {
+  //     res.render("searches/search", { facilityResults: matchingFacility });
+  //   }
+  // });
 });
 
 module.exports = router;
