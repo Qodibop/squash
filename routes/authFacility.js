4bde7463eda2917873c48d154c3ca03022ad6823
Arthur M <arthur.mossa@gmail.com>
2018-02-16 01:22:33 +0100

    Asphalt Club - Design updated 2.0 navbrs

--- routes/authFacility.js
+++ routes/authFacility.js
@@ -12,72 +12,82 @@ router.get("/signupFacility", ensureLoggedOut(), (req, res, next) => {
   res.render("authFacility/signup");
 });
 
-router.post("/signupFacility", ensureLoggedOut(), upload.single("facility-picture"), (req, res, next) => {
-  const facilityemail = req.body.facilityemail;
-  const password = req.body.password;
-  const name = req.body.name;
-  const address = req.body.address;
-  const zipcode = req.body.zipcode;
-  const postalCode = req.body.postalCode;
-  const phone = req.body.phone;
-  const description = req.body.description;
-  const openingDays = req.body.openingDays;
-  const website = req.body.website;
-  const facilityType = req.body.facilityType;
-  // const {
-  //   facilityemail,
-  //   password,
-  //   name,
-  //   address,
-  //   postalCode,
-  //   phone,
-  //   description,
-  //   openingDays,
-  //   website,
-  //   facilityType
-  // } = req.body;
+router.post(
+  "/signupFacility",
+  ensureLoggedOut(),
+  upload.single("facility-picture"),
+  (req, res, next) => {
+    const facilityemail = req.body.facilityemail;
+    const password = req.body.password;
+    const name = req.body.name;
+    const address = req.body.address;
+    const zipcode = req.body.zipcode;
+    const postalCode = req.body.postalCode;
+    const phone = req.body.phone;
+    const description = req.body.description;
+    const openingDays = req.body.openingDays;
+    const website = req.body.website;
+    const facilityType = req.body.facilityType;
+    // const {
+    //   facilityemail,
+    //   password,
+    //   name,
+    //   address,
+    //   postalCode,
+    //   phone,
+    //   description,
+    //   openingDays,
+    //   website,
+    //   facilityType
+    // } = req.body;
 
-  if (!password) {
-    req.flash("error", "Password is required");
-    return res.redirect("/signupFacility");
-  }
+    if (!password) {
+      req.flash("error", "Password is required");
+      return res.redirect("/signupFacility");
+    }
 
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    if (err) return next(err);
-    bcrypt.hash(password, salt, (err, hash) => {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
       if (err) return next(err);
-      const facility = new Facility({
-        facilityemail,
-        password: hash,
-        name,
-        picPath: req.file ? `/uploads/facility-pictures/${req.file.filename}` : null,
-        picName: req.file ? req.file.originalname : null,
-        address,
-        // postalCode,
-        phone,
-        description,
-        openingDays,
-        website,
-        facilityType,
-        zipcode
-      });
-      facility.save(err => {
-        if (err) {
-          if (err.code === 11000) {
-            req.flash("error", `The following email: ${facilityemail} already exists`);
-            res.redirect("/signupFacility");
-          } else if (facility.errors) {
-            Object.values(facility.errors).forEach(error => {
-              req.flash("error", error.message);
-            });
-            res.redirect("/signupFacility");
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) return next(err);
+        const facility = new Facility({
+          facilityemail,
+          password: hash,
+          name,
+          picPath: req.file
+            ? `/uploads/facility-pictures/${req.file.filename}`
+            : null,
+          picName: req.file ? req.file.originalname : null,
+          address,
+          // postalCode,
+          phone,
+          description,
+          openingDays,
+          website,
+          facilityType,
+          zipcode
+        });
+        facility.save(err => {
+          if (err) {
+            if (err.code === 11000) {
+              req.flash(
+                "error",
+                `The following email: ${facilityemail} already exists`
+              );
+              res.redirect("/signupFacility");
+            } else if (facility.errors) {
+              Object.values(facility.errors).forEach(error => {
+                req.flash("error", error.message);
+              });
+              res.redirect("/signupFacility");
+            }
           }
-        }
-        res.redirect("/loginFacility");
+          res.redirect("/loginFacility");
+        });
       });
     });
-  });
-});
+  }
+);
 
 router.get("/loginFacility", ensureLoggedOut(), (req, res, next) => {
   res.render("authFacility/login");
--- routes/authUser.js
+++ routes/authUser.js
@@ -13,50 +13,60 @@ router.get("/signupUser", ensureLoggedOut(), (req, res, next) => {
   res.render("authUser/signup");
 });
 
-router.post("/signupUser", ensureLoggedOut(), upload.single("user-picture"), (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const address = req.body.address;
-  const email = req.body.email;
-  const zipcode = req.body.zipcode;
+router.post(
+  "/signupUser",
+  ensureLoggedOut(),
+  upload.single("user-picture"),
+  (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const address = req.body.address;
+    const email = req.body.email;
+    const zipcode = req.body.zipcode;
 
-  if (!password) {
-    req.flash("error", "Password is required");
-    return res.redirect("/signupUser");
-  }
+    if (!password) {
+      req.flash("error", "Password is required");
+      return res.redirect("/signupUser");
+    }
 
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    if (err) return next(err);
-    bcrypt.hash(password, salt, (err, hash) => {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
       if (err) return next(err);
-      const user = new User({
-        username,
-        password: hash,
-        email,
-        picPath: req.file ? `/uploads/user-pictures/${req.file.filename}` : null,
-        picName: req.file ? req.file.originalname : null,
-        address,
-        zipcode
-      });
-
-      user.save(err => {
-        if (err) {
-          if (err.code === 11000) {
-            req.flash("error", `A user with username ${username} already exists`);
-            return res.redirect("/signupUser");
-          } else if (user.errors) {
-            Object.values(user.errors).forEach(error => {
-              req.flash("error", error.message);
-            });
-            return res.redirect("/signupUser");
-          }
-        }
+      bcrypt.hash(password, salt, (err, hash) => {
         if (err) return next(err);
-        res.redirect("/loginUser");
+        const user = new User({
+          username,
+          password: hash,
+          email,
+          picPath: req.file
+            ? `/uploads/user-pictures/${req.file.filename}`
+            : null,
+          picName: req.file ? req.file.originalname : null,
+          address,
+          zipcode
+        });
+
+        user.save(err => {
+          if (err) {
+            if (err.code === 11000) {
+              req.flash(
+                "error",
+                `A user with username ${username} already exists`
+              );
+              return res.redirect("/signupUser");
+            } else if (user.errors) {
+              Object.values(user.errors).forEach(error => {
+                req.flash("error", error.message);
+              });
+              return res.redirect("/signupUser");
+            }
+          }
+          if (err) return next(err);
+          res.redirect("/loginUser");
+        });
       });
     });
-  });
-});
+  }
+);
 
 router.get("/loginUser", ensureLoggedOut(), (req, res, next) => {
   res.render("authUser/login");
