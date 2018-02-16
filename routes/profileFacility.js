54c988ca3037767bd7ee3798f95cb2840be328ab
Arthur M <arthur.mossa@gmail.com>
2018-02-16 02:23:15 +0100

    Asphalt Club - last commit updated and working before starting editPosts

--- routes/profileFacility.js
+++ routes/profileFacility.js
@@ -10,4 +10,34 @@ router.get("/profileFacility", ensureLoggedIn(), (req, res, next) => {
   res.render("facility/profile");
 });
 
+router.get("/goodPractices", ensureLoggedIn(), (req, res, next) => {
+  res.render("facility/goodPractices");
+});
+
+// router.get("/editFacility", ensureLoggedIn(), (req, res, next) => {
+//   res.render("facility/editFacility");
+// });
+
+// router.post("/editFacility", ensureLoggedIn(), (req, res, next) => {
+//   const productId = req.params.id;
+
+//   /*
+//    * Create a new object with all of the information from the request body.
+//    * This correlates directly with the schema of Product
+//    */
+//   const updates = {
+//     name: req.body.name,
+//     price: req.body.price,
+//     imageUrl: req.body.imageUrl,
+//     description: req.body.description
+//   };
+
+//   Product.findByIdAndUpdate(productId, updates, (err, product) => {
+//     if (err) {
+//       return next(err);
+//     }
+//     return res.redirect("/products");
+//   });
+// });
+
 module.exports = router;
